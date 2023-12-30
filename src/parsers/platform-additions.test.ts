9f461625b926d2a37e4936d978bc422cfcc90207
Ronniery Borges Correa <ronnisti@gmail.com>
2023-12-30 10:11:42 +0000

    refactor: :recycle: code climate code duplication reducing

--- src/parsers/platform-additions.test.ts
+++ src/parsers/platform-additions.test.ts
@@ -3,21 +3,27 @@ import orderBy from 'lodash.orderby';
 import { PlatformAdditionsParser } from './platform-additions';
 import { newAdditions } from './platform-additions.mock';
 
-import { AddedGame } from '../types';
+import { AddedGame, GameAdditions } from '../types';
+
+type PlatformExpectation = Partial<{
+  totalOfGames: number;
+  platform: GameAdditions['platformInfo']['platform'];
+  availableCovers: GameAdditions['platformInfo']['availableCovers'];
+}>;
 
 describe('parser/platform-additions.ts PlatformAdditionsParser', () => {
   const { playstation2, playstation3, atariLynx, sega32x } = newAdditions;
 
-  it('should parse Playstation 2 additions', () => {
-    const platformAdditions = new PlatformAdditionsParser(playstation2).parse();
+  const expectPlatformParse = (html: string, expectation: PlatformExpectation): void => {
+    const additions = new PlatformAdditionsParser(html).parse();
 
-    expect(platformAdditions).toBeObject();
-    expect(platformAdditions).toHaveProperty('addedGames');
-    expect(platformAdditions).toHaveProperty('platformInfo');
+    expect(additions).toBeObject();
+    expect(additions).toHaveProperty('addedGames');
+    expect(additions).toHaveProperty('platformInfo');
 
-    const { addedGames } = platformAdditions;
+    const { addedGames } = additions;
     expect(addedGames).toBeArray();
-    expect(addedGames.length).toBe(30);
+    expect(addedGames.length).toBe(expectation.totalOfGames);
     expect(addedGames).toEqual(
       expect.arrayContaining([
         expect.objectContaining<AddedGame>({
@@ -29,91 +35,43 @@ describe('parser/platform-additions.ts PlatformAdditionsParser', () => {
       ]),
     );
 
-    const { platformInfo } = platformAdditions;
+    const { platformInfo } = additions;
     expect(platformInfo).toBeObject();
-    expect(platformInfo).toHaveProperty('platform', 'Playstation 2');
-    expect(platformInfo).toHaveProperty('availableCovers', 2003);
+    expect(platformInfo).toHaveProperty('platform', expectation.platform);
+    expect(platformInfo).toHaveProperty('availableCovers', expectation.availableCovers);
+  };
+
+  // Parsing a bunch of different platform pages, to check if the parser is not biased
+  it('should parse Playstation 2 additions', () => {
+    expectPlatformParse(playstation2, {
+      totalOfGames: 30,
+      platform: 'Playstation 2',
+      availableCovers: 2003,
+    });
   });
 
   it('should parse Playstation 3 additions', () => {
-    const platformAdditions = new PlatformAdditionsParser(playstation3).parse();
-
-    expect(platformAdditions).toBeObject();
-    expect(platformAdditions).toHaveProperty('addedGames');
-    expect(platformAdditions).toHaveProperty('platformInfo');
-
-    const { addedGames } = platformAdditions;
-    expect(addedGames).toBeArray();
-    expect(addedGames.length).toBe(30);
-    expect(addedGames).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining<AddedGame>({
-          country: expect.toBeOneOf([expect.any(String), null]),
-          dateAdded: expect.any(Date),
-          format: expect.toBeOneOf([expect.any(String), null]),
-          gameTitle: expect.toBeOneOf([expect.any(String), null]),
-        }),
-      ]),
-    );
-
-    const { platformInfo } = platformAdditions;
-    expect(platformInfo).toBeObject();
-    expect(platformInfo).toHaveProperty('platform', 'Playstation 3');
-    expect(platformInfo).toHaveProperty('availableCovers', 1275);
+    expectPlatformParse(playstation3, {
+      totalOfGames: 30,
+      platform: 'Playstation 3',
+      availableCovers: 1275,
+    });
   });
 
   it('should parse Atari Lynx additions', () => {
-    const platformAdditions = new PlatformAdditionsParser(atariLynx).parse();
-
-    expect(platformAdditions).toBeObject();
-    expect(platformAdditions).toHaveProperty('addedGames');
-    expect(platformAdditions).toHaveProperty('platformInfo');
-
-    const { addedGames } = platformAdditions;
-    expect(addedGames).toBeArray();
-    expect(addedGames.length).toBe(30);
-    expect(addedGames).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining<AddedGame>({
-          country: expect.toBeOneOf([expect.any(String), null]),
-          dateAdded: expect.any(Date),
-          format: expect.toBeOneOf([expect.any(String), null]),
-          gameTitle: expect.toBeOneOf([expect.any(String), null]),
-        }),
-      ]),
-    );
-
-    const { platformInfo } = platformAdditions;
-    expect(platformInfo).toBeObject();
-    expect(platformInfo).toHaveProperty('platform', 'Atari Lynx');
-    expect(platformInfo).toHaveProperty('availableCovers', 66);
+    expectPlatformParse(atariLynx, {
+      totalOfGames: 30,
+      platform: 'Atari Lynx',
+      availableCovers: 66,
+    });
   });
 
   it('should parse Sega 32X additions', () => {
-    const platformAdditions = new PlatformAdditionsParser(sega32x).parse();
-
-    expect(platformAdditions).toBeObject();
-    expect(platformAdditions).toHaveProperty('addedGames');
-    expect(platformAdditions).toHaveProperty('platformInfo');
-
-    const { addedGames } = platformAdditions;
-    expect(addedGames).toBeArray();
-    expect(addedGames.length).toBe(30);
-    expect(addedGames).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining<AddedGame>({
-          country: expect.toBeOneOf([expect.any(String), null]),
-          dateAdded: expect.any(Date),
-          format: expect.toBeOneOf([expect.any(String), null]),
-          gameTitle: expect.toBeOneOf([expect.any(String), null]),
-        }),
-      ]),
-    );
-
-    const { platformInfo } = platformAdditions;
-    expect(platformInfo).toBeObject();
-    expect(platformInfo).toHaveProperty('platform', 'Sega 32X');
-    expect(platformInfo).toHaveProperty('availableCovers', 57);
+    expectPlatformParse(sega32x, {
+      totalOfGames: 30,
+      platform: 'Sega 32X',
+      availableCovers: 57,
+    });
   });
 
   it('should parse the game additions ignoring empty items', () => {
