79177b3bf35d3a6f553f97a2c91ba164f8ebdc28
Ronniery Borges Correa <ronnisti@gmail.com>
2023-12-30 10:39:37 +0000

    refactor: :recycle: reduced more duplication of code

--- src/parsers/platform-additions.test.ts
+++ src/parsers/platform-additions.test.ts
@@ -3,19 +3,23 @@ import orderBy from 'lodash.orderby';
 import { PlatformAdditionsParser } from './platform-additions';
 import { newAdditions } from './platform-additions.mock';
 
-import { AddedGame, GameAdditions } from '../types';
+import { AddedGame, GameAdditions, PlatformAdditionsOptions } from '../types';
 
-type PlatformExpectation = Partial<{
-  totalOfGames: number;
-  platform: GameAdditions['platformInfo']['platform'];
-  availableCovers: GameAdditions['platformInfo']['availableCovers'];
+type ExpectPlatformOptions = Partial<{
+  parserOptions?: PlatformAdditionsOptions;
+  expectation: {
+    totalOfGames: number;
+    platform: GameAdditions['platformInfo']['platform'];
+    availableCovers: GameAdditions['platformInfo']['availableCovers'];
+  };
 }>;
 
 describe('parser/platform-additions.ts PlatformAdditionsParser', () => {
   const { playstation2, playstation3, atariLynx, sega32x } = newAdditions;
 
-  const expectPlatformParse = (html: string, expectation: PlatformExpectation): void => {
-    const additions = new PlatformAdditionsParser(html).parse();
+  const expectPlatformParse = (html: string, options: ExpectPlatformOptions): GameAdditions => {
+    const { parserOptions, expectation } = options;
+    const additions = new PlatformAdditionsParser(html).parse(parserOptions);
 
     expect(additions).toBeObject();
     expect(additions).toHaveProperty('addedGames');
@@ -23,7 +27,7 @@ describe('parser/platform-additions.ts PlatformAdditionsParser', () => {
 
     const { addedGames } = additions;
     expect(addedGames).toBeArray();
-    expect(addedGames.length).toBe(expectation.totalOfGames);
+    expect(addedGames.length).toBe(expectation?.totalOfGames);
     expect(addedGames).toEqual(
       expect.arrayContaining([
         expect.objectContaining<AddedGame>({
@@ -37,122 +41,94 @@ describe('parser/platform-additions.ts PlatformAdditionsParser', () => {
 
     const { platformInfo } = additions;
     expect(platformInfo).toBeObject();
-    expect(platformInfo).toHaveProperty('platform', expectation.platform);
-    expect(platformInfo).toHaveProperty('availableCovers', expectation.availableCovers);
+    expect(platformInfo).toHaveProperty('platform', expectation?.platform);
+    expect(platformInfo).toHaveProperty('availableCovers', expectation?.availableCovers);
+
+    return additions;
   };
 
   // Parsing a bunch of different platform pages, to check if the parser is not biased
   it('should parse Playstation 2 additions', () => {
     expectPlatformParse(playstation2, {
-      totalOfGames: 30,
-      platform: 'Playstation 2',
-      availableCovers: 2003,
+      expectation: {
+        totalOfGames: 30,
+        platform: 'Playstation 2',
+        availableCovers: 2003,
+      },
     });
   });
 
   it('should parse Playstation 3 additions', () => {
     expectPlatformParse(playstation3, {
-      totalOfGames: 30,
-      platform: 'Playstation 3',
-      availableCovers: 1275,
+      expectation: {
+        totalOfGames: 30,
+        platform: 'Playstation 3',
+        availableCovers: 1275,
+      },
     });
   });
 
   it('should parse Atari Lynx additions', () => {
     expectPlatformParse(atariLynx, {
-      totalOfGames: 30,
-      platform: 'Atari Lynx',
-      availableCovers: 66,
+      expectation: {
+        totalOfGames: 30,
+        platform: 'Atari Lynx',
+        availableCovers: 66,
+      },
     });
   });
 
   it('should parse Sega 32X additions', () => {
     expectPlatformParse(sega32x, {
-      totalOfGames: 30,
-      platform: 'Sega 32X',
-      availableCovers: 57,
+      expectation: {
+        totalOfGames: 30,
+        platform: 'Sega 32X',
+        availableCovers: 57,
+      },
     });
   });
 
   it('should parse the game additions ignoring empty items', () => {
-    const platformAdditions = new PlatformAdditionsParser(playstation3).parse({
-      ignoreEmpty: true,
+    expectPlatformParse(playstation3, {
+      parserOptions: { ignoreEmpty: true },
+      expectation: {
+        totalOfGames: 21,
+        platform: 'Playstation 3',
+        availableCovers: 1275,
+      },
     });
-
-    expect(platformAdditions).toBeObject();
-    expect(platformAdditions).toHaveProperty('addedGames');
-    expect(platformAdditions).toHaveProperty('platformInfo');
-
-    const { addedGames } = platformAdditions;
-    expect(addedGames).toBeArray();
-    expect(addedGames.length).toBe(21);
-    expect(addedGames).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining<AddedGame>({
-          country: expect.toBeOneOf([expect.any(String), null]),
-          dateAdded: expect.any(Date),
-          format: expect.toBeOneOf([expect.any(String), null]),
-          gameTitle: expect.toBeOneOf([expect.any(String), null]),
-        }),
-      ]),
-    );
-
-    const { platformInfo } = platformAdditions;
-    expect(platformInfo).toBeObject();
-    expect(platformInfo).toHaveProperty('platform', 'Playstation 3');
-    expect(platformInfo).toHaveProperty('availableCovers', 1275);
   });
 
   it('should parse the game additions ordering the result', () => {
     const unordered = new PlatformAdditionsParser(playstation3).parse();
-    const ordered = new PlatformAdditionsParser(playstation3).parse({
-      order: {
-        country: 'asc',
+    const ordered = expectPlatformParse(playstation3, {
+      parserOptions: {
+        order: {
+          country: 'asc',
+        },
+      },
+      expectation: {
+        totalOfGames: 30,
+        platform: 'Playstation 3',
+        availableCovers: 1275,
       },
     });
 
-    expect(ordered.addedGames).toBeArray();
     expect(ordered.addedGames.length).toEqual(unordered.addedGames.length);
     expect(ordered.addedGames).not.toEqual(unordered.addedGames);
     expect(orderBy(unordered.addedGames, ['country'], ['asc'])).toEqual(ordered.addedGames);
-    expect(ordered.addedGames).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining<AddedGame>({
-          country: expect.toBeOneOf([expect.any(String), null]),
-          dateAdded: expect.any(Date),
-          format: expect.toBeOneOf([expect.any(String), null]),
-          gameTitle: expect.toBeOneOf([expect.any(String), null]),
-        }),
-      ]),
-    );
   });
 
   it('should parse the game additions filtering by given date', () => {
-    const platformAdditions = new PlatformAdditionsParser(playstation3).parse({
-      startingAt: new Date('2023-09-30T23:00:00.000Z'), // october
+    expectPlatformParse(playstation3, {
+      parserOptions: {
+        startingAt: new Date('2023-09-30T23:00:00.000Z'), // october
+      },
+      expectation: {
+        totalOfGames: 16,
+        platform: 'Playstation 3',
+        availableCovers: 1275,
+      },
     });
-
-    expect(platformAdditions).toBeObject();
-    expect(platformAdditions).toHaveProperty('addedGames');
-    expect(platformAdditions).toHaveProperty('platformInfo');
-
-    const { addedGames } = platformAdditions;
-    expect(addedGames).toBeArray();
-    expect(addedGames.length).toBe(16);
-    expect(addedGames).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining<AddedGame>({
-          country: expect.toBeOneOf([expect.any(String), null]),
-          dateAdded: expect.any(Date),
-          format: expect.toBeOneOf([expect.any(String), null]),
-          gameTitle: expect.toBeOneOf([expect.any(String), null]),
-        }),
-      ]),
-    );
-
-    const { platformInfo } = platformAdditions;
-    expect(platformInfo).toBeObject();
-    expect(platformInfo).toHaveProperty('platform', 'Playstation 3');
-    expect(platformInfo).toHaveProperty('availableCovers', 1275);
   });
 });
